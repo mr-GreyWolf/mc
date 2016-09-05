3b9593c83ba4c6a9e95b31b6f20d228f0831ddfa
Andrew Borodin <aborodin@vmail.ru>
2016-09-05 14:46:20 +0300

    fixup! WST_FOCUSED: add new state and handle it.

--- lib/widget/check.c
+++ lib/widget/check.c
@@ -75,7 +75,8 @@ check_callback (Widget * w, Widget * sender, widget_msg_t msg, int parm, void *d
             return MSG_NOT_HANDLED;
         c->state ^= C_BOOL;
         c->state ^= C_CHANGE;
-        widget_set_state (w, WST_FOCUSED, TRUE);
+        widget_redraw (w);
+        send_message (w->owner, w, MSG_NOTIFY, (int) MSG_KEY, NULL);
         return MSG_HANDLED;
 
     case MSG_CURSOR:
